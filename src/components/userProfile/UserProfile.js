2b43825d6f67f84f7d4913559e9fa7a141f5ecb0
Bwardstories <thewardbunch@gmail.com>
2021-04-14 20:01:12 -0400

    simple userProfile component structure and Profile view layout and routes/links

---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserProfile = () => {
+  return <div>hello from user profile</div>;
+};
+
+export default UserProfile;
---
+++ src/views/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from "../components/userProfile/UserProfile";
+
+const Profile = () => {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+};
+
+export default Profile;
