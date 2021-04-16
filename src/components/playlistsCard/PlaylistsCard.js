2cb4d76606f31d8f26efde22d4b3d7ca8d34bf6c
Nicholas Dudash <nddudash@gmail.com>
2021-04-16 08:35:41 -0600

    actually didn't save last time :p

--- src/components/playlistsCard/PlaylistsCard.js
+++ src/components/playlistsCard/PlaylistsCard.js
@@ -7,7 +7,6 @@ import { deletePlaylists } from "../../services/backendRequests";
 import DeletionModal from "../deletionModal/DeletionModal";
 
 function PlaylistsCard(props) {
-<<<<<<< HEAD
   const user = useStore((state) => state.user);
   let setPlaylists = useStore((state) => state.setPlaylists);
 
@@ -19,9 +18,6 @@ function PlaylistsCard(props) {
       if (data.statusCode === 200) setPlaylists();
     });
   }
-=======
-  const user = useStore(state => state.user);
->>>>>>> main
 
   return (
     <Card className="playlistsCard">
