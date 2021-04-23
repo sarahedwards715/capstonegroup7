34114091416a9921841c913c70a37fb3c8104800
jeremiah harris <wonderedspark99@gmail.com>
2021-04-23 18:09:21 -0400

    looking good

--- src/components/accessCountdown/AccessCountdown.js
+++ src/components/accessCountdown/AccessCountdown.js
@@ -1,48 +1,40 @@
 import React from "react";
 import "./AccessCountdown.scss";
 import useStore from "../../store/store";
-import {
-    useEffect,
-    useState
-} from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
 function AccessCountdown() {
-    let accessExpiresIn = useStore((state) => state.accessExpiresIn);
-    let logout = useStore((state) => state.logout);
+  let accessExpiresIn = useStore((state) => state.accessExpiresIn);
+  let logout = useStore((state) => state.logout);
 
-    const [countdown, setcountdown] = useState(Number(10));
-    const [timerRunning, setTimerRunning] = useState(false);
+  const [countdown, setcountdown] = useState(accessExpiresIn);
+  const [timerRunning, setTimerRunning] = useState(false);
 
-let history = useHistory();
+  let history = useHistory();
 
-    useEffect(() => {
-        if (timerRunning) {
-            window.timer = setInterval(() => {
-                setcountdown((countdown) => countdown - 1)
-            }, 1000);
-        }
+  useEffect(() => {
+    if (timerRunning) {
+      window.timer = setInterval(() => {
+        setcountdown((countdown) => countdown - 1);
+      }, 1000);
+    }
 
-        return () => clearInterval(window.timer)
-    }, [timerRunning])
+    return () => clearInterval(window.timer);
+  }, [timerRunning]);
 
-    useEffect(() => {
-        if (countdown < 0) {
-            logout()
-            history.push("/")
-        }
-    }, [countdown])
+  useEffect(() => {
+    if (countdown < 0) {
+      logout();
+      history.push("/");
+    }
+  }, [countdown]);
 
-    useEffect(() => {
-        setTimerRunning(true)
-    }, [])
+  useEffect(() => {
+    setTimerRunning(true);
+  }, []);
 
-    return ( <
-        div className = "Accesscountdownwrapper" > {
-            countdown
-        } <
-        /div>
-    )
+  return <div className="Accesscountdownwrapper">{countdown}</div>;
 }
 
 export default AccessCountdown;
