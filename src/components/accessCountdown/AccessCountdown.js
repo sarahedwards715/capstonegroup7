5fae0c3d2293a2e87db5275f20309ecd4bd0bd44
jeremiah harris <wonderedspark99@gmail.com>
2021-04-23 17:44:47 -0400

    saving progress

---
+++ src/components/accessCountdown/AccessCountdown.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./AccessCountdown.scss";
+import useStore from "../../store/store";
+import {
+    useEffect,
+    useState
+} from "react";
+import { useHistory } from "react-router";
+
+function AccessCountdown() {
+    let accessExpiresIn = useStore((state) => state.accessExpiresIn);
+    let logout = useStore((state) => state.logout);
+
+    const [countdown, setcountdown] = useState(Number(10));
+    const [timerRunning, setTimerRunning] = useState(false);
+
+let history = useHistory();
+
+    useEffect(() => {
+        if (timerRunning) {
+            window.timer = setInterval(() => {
+                setcountdown((countdown) => countdown - 1)
+            }, 1000);
+        }
+
+        return () => clearInterval(window.timer)
+    }, [timerRunning])
+
+    useEffect(() => {
+        if (countdown < 0) {
+            logout()
+            history.push("/")
+        }
+    }, [countdown])
+
+    useEffect(() => {
+        setTimerRunning(true)
+    }, [])
+
+    return ( <
+        div className = "Accesscountdownwrapper" > {
+            countdown
+        } <
+        /div>
+    )
+}
+
+export default AccessCountdown;
